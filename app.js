6a7752ec2dae7c3259fd5fe73eb1489a592dd587
nogoh <34799883+nogoh@users.noreply.github.com>
2023-08-31 19:33:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,25 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const { WebcastPushConnection } = require('tiktok-live-connector');
 
-app.get("/", (req, res) => res.type('html').send(html));
+// Username of someone who is currently live
+let tiktokUsername = "ninkogmari";
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Create a new wrapper object and pass the username
+let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+// Connect to the chat (await can be used as well)
+tiktokLiveConnection.connect().then(state => {
+    console.info(`Connected to roomId ${state.roomId}`);
+}).catch(err => {
+    console.error('Failed to connect', err);
+})
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+// Define the events that you want to handle
+// In this case we listen to chat messages (comments)
+tiktokLiveConnection.on('chat', data => {
+    console.log(`${data.uniqueId} (userId:${data.userId}) writes: ${data.comment}`);
+})
+
+// And here we receive gifts sent to the streamer
+tiktokLiveConnection.on('gift', data => {
+    console.log(`${data.uniqueId} (userId:${data.userId}) sends ${data.giftId}`);
+})
